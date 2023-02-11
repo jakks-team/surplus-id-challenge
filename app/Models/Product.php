eb808c5dc289dea79915062d8c8933c3a8b3bb2c
Jacky Lim <jacky@somd.sg>
2023-02-12 03:46:23 +0700

    Add Products model with relationship

---
+++ app/Models/Product.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model {
+    use HasFactory;
+	/**
+	 * The table associated with the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'products';
+	/**
+	 * Indicates if the model should be timestamped.
+	 *
+	 * @var bool
+	 */
+	public $timestamps = false;
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'name',
+		'description',
+		'enable',
+	];
+	/**
+	 * Get the categories for the product.
+	 */
+	public function categories() {
+		return $this->hasManyThrough(
+			Category::class,
+			CategoryProduct::class,
+			'product_id',
+			'id',
+			'id',
+			'category_id'
+		);
+	}
+}
