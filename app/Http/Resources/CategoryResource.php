58032d6abc885b916f847976e1145fd2cfc8c993
Jacky Lim <jacky@somd.sg>
2023-02-12 14:32:45 +0700

    Modify Resource classes to handle pivot calls

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -5,6 +5,9 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class CategoryResource extends JsonResource
 {
+	protected $hiddenFields = [
+		'laravel_through_key',
+	];
     /**
      * Transform the resource into an array.
      *
@@ -13,6 +16,6 @@ class CategoryResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+		return collect(parent::toArray($request))->forget($this->hiddenFields)->toArray();
     }
 }
--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -5,14 +5,17 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProductResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
-    public function toArray($request)
-    {
-        return parent::toArray($request);
-    }
+	protected $hiddenFields = [
+		'laravel_through_key',
+	];
+	/**
+	 * Transform the resource into an array.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+	 */
+	public function toArray($request)
+	{
+		return collect(parent::toArray($request))->forget($this->hiddenFields)->toArray();
+	}
 }
